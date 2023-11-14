2b99394059095c7378b47626d9ae0cccf1fedd8a
Ethan B <bernhaen@mail.uc.edu>
2023-11-14 11:58:20 -0500

    Initial commit

---
+++ src/method.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+public class method {
+    public static void main(String[] args) {
+        double dataPoints[] = {7,8,8};
+        System.out.println("Average of data Points is: " + getAverage(dataPoints));
+    }
+    public static double getAverage(double values[]) {
+        double sum = 0;
+        double average = 0;
+        for (int x = 0; x < values.length; x++) {
+            sum += values[x];
+            average = sum / values.length;
+        }
+        return(average);
+    }
+}
+
